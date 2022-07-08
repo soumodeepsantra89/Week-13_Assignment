2dc6e9ba40761a2d069f6a275c248b5b927889e1
unknown <soumodeepsantra89@gmail.com>
2022-07-08 19:58:39 +0530

    Third Commit

---
+++ obj/Debug/netcoreapp3.1/h3dlbwwc.sql
@@ -0,0 +1,26 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Emp] (
+    [EId] int NOT NULL IDENTITY,
+    [EName] nvarchar(max) NULL,
+    [EDesig] nvarchar(max) NULL,
+    [EDOJ] datetime2 NULL,
+    CONSTRAINT [PK_Emp] PRIMARY KEY ([EId])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220708142032_FirstMig', N'3.1.25');
+
+GO
+
